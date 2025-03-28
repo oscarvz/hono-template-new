4f6ca154c2c1f76d007f54930a62b520e487ba19
oscarvz <oscarvz@users.noreply.github.com>
2025-03-28 14:47:44 +0100

    Build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { cloudflare } from '@cloudflare/vite-plugin'
-import { defineConfig } from 'vite'
-import ssrHotReload from 'vite-plugin-ssr-hot-reload'
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { defineConfig } from "vite";
+import ssrHotReload from "vite-plugin-ssr-hot-reload";
 
-export default defineConfig({
-  plugins: [ssrHotReload(), cloudflare()]
-})
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      publicDir: "./src/public",
+      build: {
+        manifest: true,
+        outDir: "./public",
+        copyPublicDir: false,
+        rollupOptions: { input: { client: "./src/client.tsx" } },
+      },
+    };
+  }
+
+  return {
+    plugins: [ssrHotReload(), cloudflare()],
+  };
+});
